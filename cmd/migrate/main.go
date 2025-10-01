0d546aa7ca3ee68e23cf76c41b82b964f271c134
titus.kariuki <titus.kariuki@volane.com>
2025-10-01 12:43:29 +0300

    feat: implement database migration commands and user table creation

---
+++ cmd/migrate/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/CodeK254/farm_app_backend/config"
+	"github.com/CodeK254/farm_app_backend/db"
+	mySQLConfig "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+	db, err := db.NewSQLStorage(mySQLConfig.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[1]
+
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+
+	if cmd == "force" {
+		if len(os.Args) < 3 {
+			log.Fatal("force command requires a version number")
+		}
+		version := os.Args[2]
+
+		// Parse version string to int
+		var versionInt int
+		if _, err := fmt.Sscanf(version, "%d", &versionInt); err != nil {
+			log.Fatal("invalid version number:", err)
+		}
+
+		if err := m.Force(versionInt); err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Forced database to version %d", versionInt)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.6
 require (
 	github.com/go-playground/validator/v10 v10.27.0
 	github.com/go-sql-driver/mysql v1.9.3
+	github.com/golang-migrate/migrate/v4 v4.19.0
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.42.0
@@ -15,6 +16,8 @@ require (
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
